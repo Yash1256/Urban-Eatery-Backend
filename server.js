e7a390d809689a24437987a1f28c279c236fd666
Yash1256 <shuklayash1256@gmail.com>
2022-01-09 19:38:50 +0530

    Added Backend

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const foodRoutes = require("./routes/foodRoute");
+const restaurantRoutes = require("./routes/restaurantRoutes");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.post("/payment", cors(), async (req, res) => {
+  let { amount, id } = req.body;
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "INR",
+      description: "Product purchase",
+      payment_method: id,
+      confirm: true,
+    });
+    console.log("Payment", payment);
+    res.json({
+      message: "Payment successful",
+      success: true,
+    });
+  } catch (error) {
+    console.log("Error", error);
+    res.json({
+      message: "Payment failed",
+      success: false,
+    });
+  }
+});
+
+const DB = process.env.DB;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB connection successful!"));
+
+app.use("/api/v1/food", foodRoutes);
+
+app.use("/api/v1/restaurant", restaurantRoutes);
+
+app.all("*", (req, res) => {
+  return res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Sever is listening on port ${port}`);
+});
